c6271154afa947efb0f2490445a3d7a05b95b772
Rezso Dubiczki <rezso.dubiczki@dreamworld.com.au>
2023-04-06 11:34:14 +1000

    tst

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Seo from "../components/seo"
 const NotFoundPage = () => {
   return (
     <>
-      <h1>404: Not Found</h1>
+      <h1>404: Not Found </h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </>
   )
